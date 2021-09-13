97dee7c5a19b9a9676b18981b1cfdabc4c73ba70
synccheng <synccheng@tencent.com>
2021-09-13 10:31:55 +0800

    ci: 使用github actions进行CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ cd public # 进入生成的文件夹
 # deploy to github
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:realduang/blog.git
+  githubUrl=git@github.com:realDuang/blog.git
 else
   msg='来自github action的自动部署'
-  githubUrl=https://realduang:${GITHUB_TOKEN}@github.com/realduang/blog.git
-  git config --global user.name "realduang"
+  githubUrl=https://realDuang:${GITHUB_TOKEN}@github.com/realDuang/blog.git
+  git config --global user.name "realDuang"
   git config --global user.email "250407778@qq.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl gh-pages # 推送到github
